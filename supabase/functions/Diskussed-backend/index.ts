9be2168305d224b0c08a6ba1fe744076bbb8f83d
Rahul Mishra <rahul.mishra@medable.com>
2022-12-27 17:56:40 +0530

    Setup base function to log url sent from extension

---
+++ supabase/functions/Diskussed-backend/index.ts
@@ -0,0 +1,32 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
+import { corsHeaders } from './cors.ts'
+
+console.log(`Function "browser-with-cors" up and running!`)
+
+serve(async (req) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  try {
+    const { url } = await req.json()
+    const data = {
+      message: `Hello ${url}!`,
+    }
+
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
