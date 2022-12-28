835a420ca8faf1fd266d6a9c327c0f67d06564fd
Rahul Mishra <rahul.mishra@medable.com>
2022-12-28 17:11:54 +0530

    .

---
+++ write_to_csv.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+START_DATE = '2022-12-27'
+
+fetch_more_page = True
+path = 'front?day=' + START_DATE
+DATA = []
+days = 1
+
+while days < 10:
+    print("Day " + str(days))
+    page = requests.get('https://news.ycombinator.com/' + path)
+    soup = BeautifulSoup(page.text)
+    stories = soup.body.center.table.find_all('tr', {"class": "athing"})
+    for story in stories:
+        storyId = "https://news.ycombinator.com/item?id=" + story.attrs['id']
+        storyUrl = story.find_all('a')[1].attrs['href']
+        # the story might be a Ask HN, or tell HN
+        if storyUrl.startswith('http'):
+            DATA.append([storyId, storyUrl])
+    more_link = soup.body.center.table.find_all('a', {"class": "morelink"})
+    if len(more_link):
+        path = more_link[0].attrs['href']
+    else:
+        # go back a page
+        days+=1
+        path = soup.find_all('span', {"class": "hnmore"})[0].a.attrs['href']
+
+print(DATA)
+
+
+# import csv
+# filename = "hn_records.csv"
+# fields=['url','discussed_url']
+# with open(filename, 'w') as csvfile:
+    
+#     csvwriter = csv.writer(csvfile)
+    
+#     csvwriter.writerow(fields)
+    
+#     csvwriter.writerows(DATA)
