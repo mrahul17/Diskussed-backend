7e5c3ba6cf8276267e5b39fb87c0a2fa9f9c52c5
Rahul Mishra <rahul.mishra@medable.com>
2022-12-30 16:56:16 +0530

    Fix indent and use batch insert helper

--- fetch_hn_stories.py
+++ fetch_hn_stories.py
@@ -1,5 +1,6 @@
 import requests
 import psycopg2
+import psycopg2.extras
 import os
 
 MAX_ID_API = "https://hacker-news.firebaseio.com/v0/maxitem.json"
@@ -24,19 +25,21 @@ def get_url_from_story(id):
         return item['url']
 
 def store_in_db(data):
-    QUERY_CURSOR.executemany("INSERT INTO discussions_v4 (url, discussed_url) VALUES (%s, %s)", data)
+    insert_sql = "INSERT INTO discussions_v4 (url, discussed_url) VALUES (%s, %s)"
+    psycopg2.extras.execute_batch(QUERY_CURSOR, insert_sql, data, page_size=100)
     DB_CONNECTION.commit()
 
 if __name__ == "__main__":
     max_hn_id = get_max_id()
     last_id_stored = get_last_id_stored()
+    print(last_id_stored, max_hn_id)
     DATA_TO_STORE_IN_DB = []
 
-for id in range(last_id_stored + 1, min((max_hn_id + 1), (last_id_stored + 500))):
-    url = get_url_from_story(id)
-    if url:
-        print(url)
-        DATA_TO_STORE_IN_DB.append(("https://news.ycombinator.com/item?id={0}".format(id), url))
+    for id in range(last_id_stored + 1, min((max_hn_id + 1), (last_id_stored + 500))):
+        url = get_url_from_story(id)
+        if url:
+            print(url)
+            DATA_TO_STORE_IN_DB.append(("https://news.ycombinator.com/item?id={0}".format(id), url))
 
     store_in_db(DATA_TO_STORE_IN_DB)
     
